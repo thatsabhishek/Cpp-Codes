d8d30f41d1c65886011e4c3b1b154836439f5980
Abhishek Gupta <83904358+Abhishek-gupta-1911@users.noreply.github.com>
2023-03-08 00:14:19 +0530

    initial commit

---
+++ bubblesort.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+using namespace std;
+
+void bubblesort(int arr[], int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main(){
+    int n, arr[20], temp;
+    cout<<"Enter the number of elements:";
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cout<<"Enter number:";
+        cin>>arr[i];
+    }
+    cout<<"Unsorted Array:";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+    
+    bubblesort(arr, n);
+
+    cout<<"Sorted Array:";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<' ';
+    }
+    return 0;
+}
